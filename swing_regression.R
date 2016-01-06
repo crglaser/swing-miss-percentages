49f7c204ea5753c6d9dacaee62b2aad4d7d496f1
Craig Glaser <crglaser@gmail.com>
2016-01-06 18:43:38 -0500

    Adding some predictors

--- swing_regression.R
+++ swing_regression.R
@@ -1,5 +1,7 @@
 swing_regression <- function(df){
   logit <- glm(swing ~ count00 + count01 + count02 + count10 + count11 + count12
-               + count20 + count21 + count22 + count30 + count31 + count32, data = df)
+               + count20 + count21 + count22 + count30 + count31 + count32 
+               + vertical_plus + vertical_minus + horizontal_plus + horizontal_minus, 
+               data = df)
   return(logit)
 }
